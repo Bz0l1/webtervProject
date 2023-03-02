642350e93c4ead4a3203389646157ceea47e3d9c
Bz0l1 <90034118+Bz0l1@users.noreply.github.com>
2023-03-02 14:25:49 +0100

    navbar

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("menuOverlay").style.height = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("menuOverlay").style.height = "0%";
+  }
