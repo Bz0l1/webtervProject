286c29c64fb9e6307558e07f0662dfb07596f44d
Bz0l1 <90034118+Bz0l1@users.noreply.github.com>
2023-03-04 11:05:22 +0100

    Updated search

--- scripts/script.js
+++ scripts/script.js
@@ -5,3 +5,36 @@ function openNav() {
  function closeNav() {
   document.getElementById("menuOverlay").style.height = "0%";
 }
+
+function openSearch() {
+  document.getElementById("searchOverlay").style.height = "100%";
+  animateSearchPlaceholder();
+}
+
+function closeSearch() {
+  document.getElementById("searchOverlay").style.height = "0%";
+  clearInterval(searchInterval);
+}
+
+function animateSearchPlaceholder() {
+  var placeholder = "Keresés";
+  var count = 0;
+  searchInterval = setInterval(function() {
+    count++;
+    switch(count % 4) {
+      case 1:
+        placeholder += ".";
+        break;
+      case 2:
+        placeholder += ".";
+        break;
+      case 3:
+        placeholder += ".";
+        break;
+      default:
+        placeholder = "Keresés";
+        break;
+    }
+    document.getElementById("overlaySearch").setAttribute("placeholder", placeholder);
+  }, 500);
+}
