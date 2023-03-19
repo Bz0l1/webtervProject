f5034ac6aed0e69f788c2c8b37d8501708e87cd4
Bz0l1 <90034118+Bz0l1@users.noreply.github.com>
2023-03-19 11:36:58 +0100

    filmsorozat

--- scripts/script.js
+++ scripts/script.js
@@ -39,20 +39,43 @@ function animateSearchPlaceholder() {
   }, 500);
 }
 
-function replaceImage(slide) {  
-  var posterImage = document.getElementById("posterIMG");
-  var mainTitle = document.getElementById("mainTitle");
-  var description = document.querySelector(".description p");
-  var slideImage = slide.querySelector("img");
-  var slideDesc = slide.querySelector(".slide-desc");
-  var imageSrc = slideImage.getAttribute("src");
-  var descText = slideDesc.textContent;
-  var titleText = mainTitle.textContent;
-  var tempImageSrc = posterImage.getAttribute("src");
+function sortTable(n, tableIndex = 0) {
+  let table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementsByClassName("film-table")[tableIndex];
+  switching = true;
+  dir = "asc";
 
-  posterImage.setAttribute("src", imageSrc);
-  slideImage.setAttribute("src", tempImageSrc);
-  mainTitle.textContent = descText;
-  slideDesc.textContent = titleText;
-  description.textContent = descText;
+  while (switching) {
+      switching = false;
+      rows = table.rows;
+
+      for (i = 1; i < rows.length - 1; i++) {
+          shouldSwitch = false;
+          x = rows[i].getElementsByTagName("TD")[n];
+          y = rows[i + 1].getElementsByTagName("TD")[n];
+
+          if (dir === "asc") {
+              if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                  shouldSwitch = true;
+                  break;
+              }
+          } else if (dir === "desc") {
+              if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                  shouldSwitch = true;
+                  break;
+              }
+          }
+      }
+
+      if (shouldSwitch) {
+          rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+          switching = true;
+          switchcount++;
+      } else {
+          if (switchcount === 0 && dir === "asc") {
+              dir = "desc";
+              switching = true;
+          }
+      }
+  }
 }
